b3028e92c2915a2d1bc41cd5cd5d24cab4a1ae61
vgshenshin <vg.shenshin@gmail.com>
2023-05-03 11:29:11 +0300

    install ReactRouter 5.3.4 and add Router in App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import ComicsList from "../comicsList/ComicsList";
+import AppBanner from "../appBanner/AppBanner";
 
 import decoration from '../../resources/img/vision.png';
 
@@ -18,24 +21,33 @@ const App = () => {
 	}
 
 	return (
-		<div className="app">
-			<AppHeader/>
-			<main>
-				{/* <ErrorBoundary>
-					<RandomChar/>
-				</ErrorBoundary>
-				<div className="char__content">
-					<ErrorBoundary>
-						<CharList onCharSelected={onCharSelected} />
-					</ErrorBoundary>
-					<ErrorBoundary>
-						<CharInfo charId={selectedChar} />
-					</ErrorBoundary>
-				</div>
-				<img className="bg-decoration" src={decoration} alt="vision"/> */}
-				<ComicsList/>
-			</main>
-		</div>
+		<Router>
+			<div className="app">
+				<AppHeader/>
+				<main>
+					<Switch>
+						<Route exact path="/">
+							<ErrorBoundary>
+								<RandomChar/>
+							</ErrorBoundary>
+							<div className="char__content">
+								<ErrorBoundary>
+									<CharList onCharSelected={onCharSelected} />
+								</ErrorBoundary>
+								<ErrorBoundary>
+									<CharInfo charId={selectedChar} />
+								</ErrorBoundary>
+							</div>
+							<img className="bg-decoration" src={decoration} alt="vision"/>
+						</Route>
+						<Route exact path="/comics">
+							<AppBanner/>
+							<ComicsList/>
+						</Route>
+					</Switch>
+				</main>
+			</div>
+		</Router>
 	)
 }
 

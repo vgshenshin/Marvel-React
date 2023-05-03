a689c84b5fdc75feff9954d54028253548d6f561
vgshenshin <vg.shenshin@gmail.com>
2023-05-03 12:28:48 +0300

    refactor App, created components for pages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,11 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import ComicsList from "../comicsList/ComicsList";
-import AppBanner from "../appBanner/AppBanner";
 
-import decoration from '../../resources/img/vision.png';
+import { MainPage, ComicsPage } from "../pages";
+
 
 const App = () => {
-
-	const [selectedChar, setSelectedChar] = useState(null);
-
-	// метод для записи в стэйт id персонажа из компонента CharList
-	const onCharSelected = (id) => {
-		setSelectedChar(id)
-	}
 
 	return (
 		<Router>
@@ -27,22 +14,10 @@ const App = () => {
 				<main>
 					<Switch>
 						<Route exact path="/">
-							<ErrorBoundary>
-								<RandomChar/>
-							</ErrorBoundary>
-							<div className="char__content">
-								<ErrorBoundary>
-									<CharList onCharSelected={onCharSelected} />
-								</ErrorBoundary>
-								<ErrorBoundary>
-									<CharInfo charId={selectedChar} />
-								</ErrorBoundary>
-							</div>
-							<img className="bg-decoration" src={decoration} alt="vision"/>
+							<MainPage/>
 						</Route>
 						<Route exact path="/comics">
-							<AppBanner/>
-							<ComicsList/>
+							<ComicsPage/>
 						</Route>
 					</Switch>
 				</main>
